c0c589ec3e115bf282ebe89aedfb604afd76758f
Romário Lima <devromariolima@gmail.com>
2024-07-17 17:45:39 -0300

    Ajustes na main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,4 @@ void main() {
 }
 
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Text('Contador: $counter'),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
-      )),
-    );
-  }
-}
